45281b7df2d95824a43b64fa90ca6342b3adfff8
unknown <arnav22112003@gmail.com>
2022-11-06 12:46:45 +0530

    responsive improved

--- scrypt.js
+++ scrypt.js
@@ -9,7 +9,6 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    console.log("hello")
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
